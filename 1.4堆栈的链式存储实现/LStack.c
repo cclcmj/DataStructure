f53100eb888acd1288648f62a048d20e1025d473
ccclcmj <MJ1666812276@outlook.com>
2019-01-10 16:41:54 +0800

    堆栈的链式存储实现

---
+++ 1.4堆栈的链式存储实现/LStack.c
@@ -0,0 +1,55 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <DataStructure.h>
+#include <LStack.h>
+
+LStack CreateLStack()
+{
+    LStack S=(LStack)malloc(sizeof(struct LSNode));
+    S->Next=NULL;
+    return S;
+}
+int IsEmpty(LStack S)
+{
+    if(S->Next)
+        return 0;
+    else
+        return 1;
+}
+void Push(ElementType item,LStack S)
+{
+    LStack n=(LStack)malloc(sizeof(struct LSNode));
+    n->Data=item;
+    n->Next=S->Next;
+    S->Next=n;
+}
+ElementType Pop(LStack S)
+{
+    if(IsEmpty(S))
+    {
+        printf("栈满");
+        return NULL;
+    }
+    else
+    {
+        int i;
+        LStack n=(LStack)malloc(sizeof(struct LSNode));
+        n=S->Next;
+        i=n->Data;
+        S->Next=n->Next;
+        free(n);
+        return i;
+    }
+}
+
+void printLStack(LStack S)
+{
+    LStack pos=S->Next;
+    while(pos)
+    {
+        printf("%d,",pos->Data);
+        pos=pos->Next;
+    }
+    printf("\n");
+    return;
+}
---
+++ 1.4堆栈的链式存储实现/LStack.h
@@ -0,0 +1,21 @@
+﻿#ifndef LSTACK_H_INCLUDED
+#define LSTACK_H_INCLUDED
+#include <DataStructure.h>
+
+/*
+堆栈的链式存储实现
+*/
+typedef struct LSNode *LStack;
+struct LSNode
+{
+    ElementType Data;
+    LStack Next;
+};
+
+LStack CreateLStack();
+int IsEmpty(LStack S);
+void Push(ElementType item,LStack S);
+ElementType Pop(LStack S);//判断栈空
+
+void printLStack(LStack S);//输出栈的全部元素
+#endif // LSTACK_H_INCLUDED
