c4f3259dfaad90731ad3ab6edf85c89b17b4aa4d
ccclcmj <MJ1666812276@outlook.com>
2019-01-23 14:07:30 +0800

    树的非递归遍历

--- 2.2二叉树的链式存储实现/LBinTree.c
+++ 2.2二叉树的链式存储实现/LBinTree.c
@@ -13,7 +13,72 @@ bool IsEmpty(LBinTree BT)
 {
     return (BT->left==NULL);
 }
-
+void PreOrderTraversalST(LBinTree T)
+{
+    Stack S=CreateStack();
+    LBinTree pos=T->left;
+    while(pos||!IsEmptyS(S)){
+        while(pos){
+            printf("%d,",pos->Data);
+            Push(pos,S);
+            pos=pos->left;
+        }
+        if(!IsEmptyS(S)){
+            pos=Pop(S);
+            pos=pos->right;
+        }
+    }
+    free(S);
+    printf("\n");
+    return;
+}
+void InOrderTraversalST(LBinTree BT)
+{
+    Stack S=CreateStack();
+    LBinTree pos=BT->left;
+    while(pos||!IsEmptyS(S)){
+        while(pos){
+            Push(pos,S);
+            pos=pos->left;
+        }
+        if(!IsEmptyS(S)){
+            pos=Pop(S);
+            printf("%d,",pos->Data);
+            pos=pos->right;
+        }
+    }
+    free(S);
+    printf("\n");
+    return;
+}
+void PosOrderTraversalST(LBinTree BT)
+{
+    Stack S =CreateStack();
+    LBinTree pos=BT->left;LBinTree LastPos;
+    if(IsEmpty(BT)){
+        printf("树空");
+        return;
+    }
+    Push(pos,S);
+    while(!IsEmptyS(S)){
+        pos=NULL;
+        pos=Pop(S);
+        Push(pos,S);
+        if((pos->left==NULL&&pos->right==NULL)||(LastPos!=NULL&&(LastPos==pos->right))||(LastPos!=NULL&&(pos->right==NULL&&LastPos==pos->left)))
+        {
+            printf("%d,",pos->Data);
+            LastPos=pos;
+            pos=Pop(S);
+        }else if(pos->left!=NULL&&LastPos!=pos->left){
+            Push(pos->left,S);
+        }else if(pos->right!=NULL){
+            Push(pos->right,S);
+        }
+    }
+    free(S);
+    printf("\n");
+    return;
+}
 
 void AddNode(Position last,enum LorR lr,ElementType item)
 {
@@ -48,4 +113,36 @@ ElementType DeleteNode(Position last,enum LorR lr)
         printf("删除的节点不存在\n");
         return -1;
     }
+}
+
+Stack CreateStack()
+{
+    Stack n=(Stack)malloc(sizeof(struct SNode));
+    n->Data=NULL;
+    n->Next=NULL;
+    return n;
+}
+bool IsEmptyS(Stack S){
+    if(S->Next==NULL)
+        return true;
+    else return false;
+}
+void Push(Position Tpos,Stack S)
+{
+    Stack n=(Stack)malloc(sizeof(struct SNode));
+    n->Next=S->Next;
+    S->Next=n;
+    n->Data=Tpos;
+}
+Position Pop(Stack S)
+{
+    if(IsEmptyS(S)){
+        printf("栈空");
+        return NULL;
+    }
+    Stack Spos=S->Next;
+    Position Tpos=Spos->Data;
+    S->Next=Spos->Next;
+    free(Spos);
+    return Tpos;
 }
--- 2.2二叉树的链式存储实现/LBinTree.h
+++ 2.2二叉树的链式存储实现/LBinTree.h
@@ -14,11 +14,27 @@ struct LTreeNode
 
 LBinTree CreateLBinTree();
 bool IsEmpty(LBinTree Bt);
-void TraversalST(LBinTree BT);
+void PreOrderTraversalST(LBinTree BT);//先序非递归遍历
+void InOrderTraversalST(LBinTree BT);//中序非递归遍历
+void PosOrderTraversalST(LBinTree BT);//后序非递归遍历
+
 
 //last指向的节点只能是叶节点
 enum LorR {right,left};
 void AddNode(Position last,enum LorR lr,ElementType item);
 ElementType DeleteNode(Position last,enum LorR lr);
+
+
+//遍历用到的堆栈
+typedef struct SNode *Stack;
+struct SNode
+{
+    Position Data;
+    Stack Next;
+};
+Stack CreateStack();
+bool IsEmptyS(Stack S);
+void Push(Position pos,Stack S);
+Position Pop(Stack S);
 
 #endif // LBINTREE_H_INCLUDED
