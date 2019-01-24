e06ab3377a5f55babf070749778dd2622a580e3e
ccclcmj <MJ1666812276@outlook.com>
2019-01-24 23:27:03 +0800

    Create BinarySearchTree.h
    二叉搜索树的头文件

---
+++ 2.3二叉搜索树的实现/BinarySearchTree.h
@@ -0,0 +1,21 @@
+#ifndef BINARYSEARCHTREE_H_INCLUDED
+#define BINARYSEARCHTREE_H_INCLUDED
+#include <DataStructure.h>
+#include <stdbool.h>
+
+typedef struct BSTreeNode *BSTree;
+typedef BSTree Position;
+struct BSTree
+{
+    ElementType Data;
+    BSTree left;
+    BSTree right;
+};
+//测试
+Position Find(ElementType X,BSTree BST);
+Position FindMin(BSTree BST);
+Position FindMax(BSTree BST);
+BSTree Insert(ElementType X,BSTree BST);
+BSTree Delete(ElementType X,BSTree BST);
+
+#endif // BINARYSEARCHTREE_H_INCLUDED
